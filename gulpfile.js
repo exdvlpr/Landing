cbb88699c8f8cc04658bffdf729e41953ec0ef87
Vlad Dyubov <exdvlpr@gmail.com>
2017-10-21 20:10:36 +0600

    Form js added

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,25 @@ const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
+
+/*-------js--------------*/
+gulp.task('js', function () {
+    return gulp.src([
+        'source/js/form.js',
+        'source/js/navigation.js',
+        'source/js/main.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'));
+
+})
 /*-----Server----*/
 gulp.task('server', function() {
     browserSync.init({
@@ -72,11 +89,12 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
     gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
     gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+    gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series(
     'clean',
-    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+    gulp.parallel('templates:compile', 'styles:compile','js', 'sprite', 'copy'),
     gulp.parallel('watch', 'server')
     )
 );
